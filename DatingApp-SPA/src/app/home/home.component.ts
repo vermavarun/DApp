9a6cab53a8d8fc4eba702502280ea1ce3dc42405
Varun Verma <varun.verma@nagarro.com>
2018-11-10 21:36:14 +0530

    End of section 7

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -9,13 +9,13 @@ import { HttpClient } from '@angular/common/http';
 export class HomeComponent implements OnInit {
   registerMode = false;
 
-  constructor(private http: HttpClient) {}
-  
+  constructor(private http: HttpClient) { }
+
   ngOnInit() {
   }
 
 
-  registerToggle(){
+  registerToggle() {
     this.registerMode = true;
   }
 
